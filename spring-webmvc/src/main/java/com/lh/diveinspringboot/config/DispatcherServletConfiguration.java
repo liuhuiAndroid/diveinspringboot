50c0d515f0e0f406c286dab53b9904f2a7d3113e
lh <642404044@qq.com>
2018-09-17 17:34:21 +0800

    Initial commit

---
+++ spring-webmvc/src/main/java/com/lh/diveinspringboot/config/DispatcherServletConfiguration.java
@@ -0,0 +1,7 @@
+package com.lh.diveinspringboot.config;
+
+import org.springframework.context.annotation.ComponentScan;
+
+//@ComponentScan(basePackages = "com.lh.diveinspringboot.config")
+public class DispatcherServletConfiguration {
+}
